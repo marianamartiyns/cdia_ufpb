d552f67155b4bd86599057a2f1004da498cd95d9
Mariana Martins <marianamartiyns@gmail.com>
2022-10-08 09:41:52 -0300

    update

---
+++ ip_p1_bruno/lista4/ex004.py
@@ -0,0 +1,10 @@
+n1 = input("Digite um número: ")
+n2 = input("Digite um número: ")
+
+p1 = n1 // n2
+p2 = n1 - n2 * (n1 % n2)
+
+print(p1 , p2)
+
+# print - n1 // n2
+# n1 % n2 == n1 - n2 * (n1 % n2) 
