5fa60a3433eac9ae4690f474da0c00766eff78b0
Mariana Martins <marianamartiyns@gmail.com>
2022-10-12 15:30:33 -0300

    update

--- ip_p1_bruno/lista4/ex005.py
+++ ip_p1_bruno/lista4/ex005.py
@@ -1,28 +1,27 @@
 # Leitura dos Dados Referente a uma Dívida e Cálculo dos Meses até a Mesma ser Quitada.
 
-divida = float(input("\nQual o Valor Inicial da sua Dívida? ")) # Valor Inicial
+divida = float(input("Qual o Valor Inicial da sua Dívida? ")) # Valor Inicial
 
-jurm = float(input("Qual é o Juros Mensal? (ex: 3% = 0.03): ")) # Juros por Mês
+jurm = float(input("Qual é o Juros Mensal? (ex: 3% = 0.03) ")) # Juros por Mês
 
 parcela = float(input("Quanto será pago por mês? ")) # Pagamento por Mês
 
+total = c = 0 # c para contador
 meses = divida / parcela
-saldo = divida
-juros = total = c = 0
 
 
 if (divida * jurm) > parcela:
     print("Valor Não Compatível")
     
 else: 
-    while meses != juros:
-        parcela = parcela + (parcela + juros)
+    while meses != c:
+        parcela = parcela + (parcela + jurm)
         total = total + parcela
-        c += 1
-    
-    saldo = saldo + juros
+        c = c + 1
 
-print(f"A divida poderá ser paga em {meses} meses, com um total pago de {saldo} e o total de juros de {c}")
+    total_juros = total - divida
+
+print(f"A dívida poderá ser paga em {meses:.1f} meses, com um total pago de R${total} sendo R${total_juros} o total de juros.")
 
 
 
