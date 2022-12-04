6aa6d4a4cd5e2311882934ccb203e79d16a5f771
Mariana Martins <marianamartiyns@gmail.com>
2022-12-04 03:08:09 -0300

    up

--- ip_p1_bruno/lista2/ex1.py
+++ ip_p1_bruno/lista2/ex1.py
@@ -3,6 +3,6 @@ print("\n-- Código para Cálculo da Área de um Círculo a partir do Raio --")
 raio = float(input('Digite o raio do círculo: '))
 
 # Cálculo da Área do Círculo
-a = 3.14 * (raio * 2)
+a = 3.14 * (raio ** 2)
 
 print(f"O círculo com o raio de {raio} tem uma área de {a:.1f} .")
