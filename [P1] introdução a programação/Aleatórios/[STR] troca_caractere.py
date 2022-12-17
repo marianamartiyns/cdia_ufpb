63d9151fcdf90df79b53f84ff4d409e32f7f4fa3
Mariana Martins <marianamartiyns@gmail.com>
2022-12-17 01:02:21 -0300

    update

---
+++ [P1] introdução a programação/Aleatórios/[STR] troca_caractere.py
@@ -0,0 +1,21 @@
+# Faça um programa que troque a inicial da frase por um caractere ao longo da frase:
+# ex: A copa flopou geral == # cop# flopou ger#l
+
+print("\n== TROCA STRING ==")
+
+frase = str(input("Digite uma palavra: "))
+caractere = str(input("Digite um caractere para substituir: "))
+
+frase_final = ''
+
+letra = frase[0]
+
+for i in range (len(frase)):
+    
+    if frase[i] != letra:
+        frase_final += frase[i]
+        
+    else:
+        frase_final += caractere
+        
+print(frase_final)
