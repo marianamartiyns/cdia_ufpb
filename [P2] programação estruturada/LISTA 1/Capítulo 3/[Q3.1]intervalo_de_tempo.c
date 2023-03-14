905c89dab0d6771593ce64b2b7d742b5ccb996bf
Mariana Martins <marianamartiyns@gmail.com>
2023-03-14 10:48:23 -0300

    update

--- [P2] programação estruturada/LISTA 1/Capítulo 3/[Q3.1]intervalo_de_tempo.c
+++ [P2] programação estruturada/LISTA 1/Capítulo 3/[Q3.1]intervalo_de_tempo.c
@@ -1,33 +1,42 @@
 /*Programa que determina o intervalo de tempo decorrido entre dois instantes*/
+// RESCREVER PARA RECEBR HORA MINUTOS E SEGUNDOS
 
 #include <stdio.h>
-
-// RESCREVER PARA RECEBR HORA MINUTOS E SEGUNDOS
+#include <stdlib.h>
+#include <locale.h>
 
 int main(){
 
-    int h1, min1, h2, min2, h, min;
+    setlocale(LC_ALL, "Portuguese");
 
-    printf("Digite o instante inicial (horas e minutos)");
-    scanf("%d %d", &h1, &min1);
+    int h1, min1, seg1, h2, min2, seg2, h, min, seg;
 
-    printf("Digite o instante final");
-    scanf("%d %d", &h2, &min2);
+    printf("\nDigite o instante inicial em horas, minutos e segundos(ex: 12:43:52 = 12 43 52): ");
+    scanf("%d %d %d", &h1, &min1, &seg1);
+
+    printf("\nDigite o instante final em horas, minutos e segundos(ex: 05:25:13 = 05 25 13): ");
+    scanf("%d %d %d", &h2, &min2, &seg2);
 
     h = h2 - h1;
     min = min2 - min1;
+    seg = seg1 - seg2;
 
-    if ((h < 0) || ((h == 0) && (min < 0)))
-    printf("\aDados invalidos! O segundo instante é anterior ao primeiro");
+    if ( (h < 0) || (h == 0) && (min < 0) || (min == 0) && (seg < 0) )
+            printf("\nDados invalídos! O segundo instante é anterior ao primeiro.");
 
     else{
 
-        if (min < 0){
-            h = h - 1;
-            min = min + 60;
+            if (min < 0){
+                h = h - 1;
+                min = min + 60;
             }
 
-        printf( "Entre os instantes %dh %dmin e %dh %dmin passaram-se %dh %dmin", h1, min1, h2, min2, h, min);
+            if (seg < 0){
+                min = min - 1;
+                seg = seg + 60;
+            }
+
+        printf( "\nEntre os instantes %d:%d:%d e %d:%d:%d passaram-se %d hr(s), %d min(s) e %d seg(s).\n", h1, min1, seg1, h2, min2, seg2, h, min, seg);
     }
 }
 
