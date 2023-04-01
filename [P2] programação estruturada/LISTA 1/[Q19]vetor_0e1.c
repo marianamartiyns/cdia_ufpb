dab30a9b04b68a95da0d1d574d5d627d0042d415
Mariana Martins <marianamartiyns@gmail.com>
2023-04-01 13:26:38 -0300

    update

--- [P2] programação estruturada/LISTA 1/[Q19]vetor_0e1.c
+++ [P2] programação estruturada/LISTA 1/[Q19]vetor_0e1.c
@@ -22,29 +22,19 @@ int main() {
         printf("\nDigite o %dº elemento do vetor: ", i+1);
         scanf("%d", &vetor_original[i]); }
 
-    // copia vetor_original para vetor_copia
     for (i = 0; i < 10; i++) {
         vetor_copia[i] = vetor_original[i];
     }
 
-    // exibe vetor_original antes da modificacao
     printf("\nVetor original: ");
     for (i = 0; i < 10; i++)
         printf("%d ", vetor_original[i]);
-
     printf("\n");
 
-    // chama funcao para substituir valores par/impares
     substituiParImpar(vetor_copia, 10);
-
-    // exibe vetor_copia depois da modificacao
-    printf("\nVetor modificado: ");
+    printf("Vetor modificado: ");
     for (i = 0; i < 10; i++) {
         printf("%d ", vetor_copia[i]);
     }
     printf("\n");
-
-    return 0;
 }
-
-
