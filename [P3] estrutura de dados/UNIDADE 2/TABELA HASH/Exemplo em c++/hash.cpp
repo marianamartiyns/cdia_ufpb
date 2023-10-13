e5aa7f346e562931286991414665a365a2ecdf3d
Mariana Martins <marianamartiyns@gmail.com>
2023-10-13 18:45:36 -0300

    update

--- [P3] estrutura de dados/UNIDADE 2/TABELA HASH/Exemplo em c++/hash.cpp
+++ [P3] estrutura de dados/UNIDADE 2/TABELA HASH/Exemplo em c++/hash.cpp
@@ -1,5 +1,4 @@
-#include "hash.h"
-//#include "aluno.h"
+#include "tudao.h"
 #include <iostream>
 
 using namespace std;
@@ -56,3 +55,17 @@ int Hash::get_hash(Aluno aluno){
     return aluno.get_ra() % max_items;
 }
 
+int main(){
+
+    Hash alunoHash(10);
+
+    int ras[7] = {63814, 54936, 13285, 14283, 10723, 15892, 71659};
+    string nomes[7] = {"Rafaela", "Mariana", "Jeova", "Bruno", "Felly", "cLAY", "Carla"};
+
+for (int i = 0; i< 7; i++){
+    Aluno aluno = Aluno(ras[i], nomes[i]);
+    alunoHash.insert_item(aluno);
+    }
+alunoHash.print();
+
+}
--- [P3] estrutura de dados/UNIDADE 2/TABELA HASH/Exemplo em c++/hash.h
+++ [P3] estrutura de dados/UNIDADE 2/TABELA HASH/Exemplo em c++/hash.h
@@ -12,14 +12,14 @@ class Aluno{
     string nome;
 
 public:
-    Aluno(); // construtor sem parametros para a alocação dinamica de memoria
+    Aluno(); // construtor sem parametros para a alocacao dinamica de memoria
     Aluno(int ra, string nome);
     string get_nome() const;
     int get_ra() const;
 };
 
 Aluno::Aluno (){
-    this -> ra = -1; // a posição no vetor está vazia
+    this -> ra = -1; // a posicao no vetor esta vazia
     this -> nome = "";
 };
 
@@ -31,3 +31,23 @@ Aluno::Aluno(int ra, string nome){
 string Aluno:: get_nome() const { return nome; }
 int Aluno::get_ra() const { return ra; }
 
+
+class Hash{
+
+private:
+    int get_hash(Aluno aluno);
+    int max_items;
+    int length;
+    Aluno* structure; // um vetor de objetos aluno
+
+public:
+    Hash(int max_items = 100);
+    ~Hash();
+    bool is_full() const;
+    int get_length() const;
+
+    void retrieve_item(Aluno& aluno, bool& found);
+    void insert_item(Aluno aluno);
+    void delete_item(Aluno aluno);
+    void print();
+};
