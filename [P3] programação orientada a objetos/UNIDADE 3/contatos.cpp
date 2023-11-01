6629b8dc804719953482819ea2f5ea9bd5da0a7e
Mariana Martins <marianamartiyns@gmail.com>
2023-10-31 21:51:48 -0300

    update

--- [P3] programação orientada a objetos/UNIDADE 3/contatos.cpp
+++ [P3] programação orientada a objetos/UNIDADE 3/contatos.cpp
@@ -3,3 +3,77 @@
 Crie uma classe `Contato` que armazena informações de nome, telefone e email de uma pessoa.
 Implemente um sistema de gerenciamento de contatos que permita adicionar, remover e listar contatos.
 Utilize um `std::vector` para armazenar os objetos `Contato`. */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class contato {
+
+    string nome;
+    string telefone;
+    string email;
+
+public:
+
+    contato(const string& n, const string& t, const string& e) : nome(n), telefone(t), email(e) {}
+
+    string get_nome(){ return nome; }
+    string get_telefone() { return telefone; }
+    string get_email() { return email; }
+
+    void set_nome(string n) { nome = n ;}
+    void set_telefone(string t){ telefone = t; }
+    void set_email(string e) { email = e; }
+
+};
+
+class agenda {
+
+    vector <contato> contatos;
+public:
+
+    void adicionar_contato(const contato& c) {
+        contatos.push_back(c);
+    }
+
+    void listar_contatos(){
+        for (contato& c: contatos){
+            cout << "Nome: " << c.get_nome();
+            cout << " | Telefone: " << c.get_telefone();
+            cout << " | E-mail: " << c.get_email() << endl;
+        }
+    }
+
+    void remover_contato(const string n) {
+
+        for (auto it = contatos.begin(); it != contatos.end(); ) {
+
+                if ((*it).get_nome() == n) {
+                        it = contatos.erase(it); //remove elemento
+                } else { ++ it; }
+        }
+    }
+};
+
+int main(void) {
+
+    agenda minha_agenda;
+
+    minha_agenda.adicionar_contato(contato("Mariana", "45678965", "mariana@gmail.com"));
+    minha_agenda.adicionar_contato(contato("Jeova", "839362456", "jeova123@gmail.com"));
+    minha_agenda.adicionar_contato(contato("Felipe", "3456781", "felly@gmail.com"));
+    minha_agenda.adicionar_contato(contato("Cleydson", "2678123", "juninhoclay@gmail.com"));
+
+    minha_agenda.listar_contatos();
+
+    cout << endl;
+    cout << "Removendo Contato..." << endl;
+    minha_agenda.remover_contato("Cleydson");
+    cout << endl;
+
+    minha_agenda.listar_contatos();
+
+return 0; }
--- [P3] programação orientada a objetos/UNIDADE 3/to-do_list.cpp
+++ [P3] programação orientada a objetos/UNIDADE 3/to-do_list.cpp
@@ -1,4 +1,95 @@
 /* Exercício 4: Gerenciador de Tarefas (To-Do List)**
 
-Crie uma classe `Tarefa` que armazena informações sobre tarefas, como descrição, data de vencimento e estado (concluída ou não). Implemente um gerenciador de tarefas que permita adicionar, remover, listar e marcar tarefas como concluídas.
+Crie uma classe `Tarefa` que armazena informações sobre tarefas, como descrição, data de vencimento e estado (concluída ou não).
+Implemente um gerenciador de tarefas que permita adicionar, remover, listar e marcar tarefas como concluídas.
 Utilize um `std::list` para armazenar objetos `Tarefa`. */
+
+#include <iostream>
+#include <string>
+#include <list>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::list;
+
+class tarefa {
+	string descricao;
+	string deadline;
+	bool concluida;
+public:
+	tarefa(const string& desc, const string& dead, bool c) :
+		descricao(desc), deadline(dead), concluida(c) { }
+	const string& get_descricao() { return descricao; }
+	const string& get_deadline() { return deadline; }
+
+	bool is_concluida() { return concluida; }
+
+	void set_concluida(bool c) {
+		concluida = c;
+	}
+};
+
+class gerenciador_tarefas {
+	list<tarefa> tarefas;
+public:
+	void adicionar_tarefa(const tarefa& t) {
+		tarefas.push_back(t);
+	}
+
+	void listar_tarefas() {
+		int counter = 1;
+		for (tarefa& t : tarefas) {
+			cout << "(" << counter++ << ") ";
+			cout << t.get_descricao() << " | ";
+			cout << t.get_deadline() << " | ";
+			cout << (t.is_concluida()?"[Concluida]":"[Pendente]");
+			cout << endl;
+		}
+
+	}
+
+	void remover_tarefa(int n) {
+		if (n >= 1 && n <= tarefas.size()) {
+			auto it = tarefas.begin();
+			std::advance(it, n - 1);
+			tarefas.erase(it);
+		}
+
+	}
+
+	void definir_tarefa_concluida(int n) {
+		if (n >= 1 && n <= tarefas.size()) {
+			auto it = tarefas.begin();
+			std::advance(it, n - 1);
+			it->set_concluida(true);
+		}
+
+	}
+
+};
+
+int main(void) {
+	gerenciador_tarefas gt;
+
+	gt.adicionar_tarefa(tarefa("Fazer a feira", "26/10/23", false));
+	gt.adicionar_tarefa(tarefa("Arrumar a casa", "27/10/23", false));
+	gt.adicionar_tarefa(tarefa("Corrigir as provas", "28/10/23", false));
+	gt.adicionar_tarefa(tarefa("Consertar o carro", "31/10/23", false));
+
+	gt.listar_tarefas();
+
+	cout << endl;
+	cout << "Removendo tarefa 4 e concluindo tarefa 3" << endl;
+
+	gt.remover_tarefa(4);
+	gt.definir_tarefa_concluida(3);
+
+	cout << endl;
+
+	gt.listar_tarefas();
+
+	return 0;
+
+}
